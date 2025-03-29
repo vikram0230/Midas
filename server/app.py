b6d50289fd00a98e9466f148de8c74d70a415d78
unknown <anirudhkuppili@gmail.com>
2025-03-29 13:28:04 -0500

    Initializedd server

---
+++ server/app.py
@@ -0,0 +1,18 @@
+
+
+# Endpoint 1 (POST) - Predict future data (Params: start_time_future, end_time_future)
+    # Flow
+    # 1. Call Prediction Model with parameters
+    # 2. Return data
+
+# Endpoint 2 (POST) - Realtime Insert data into convex db (Params: data)
+    # Flow
+    # 1. Get data from Plaid Real Time
+    # 2. Call categorizer model to categorize data
+    # 3. Insert data into convex db
+
+# Endpoint 3 (POST) - What if analysis (Params: data)
+    # Flow
+    # 1. Get data from request on decrease, increase, and no change
+    # 2. Call prediction model to predict future data with parameters
+    # 3. Return data
